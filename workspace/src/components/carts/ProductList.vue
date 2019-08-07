a60500ab4404c50f830772c37ac467a4ebdce094
zzx <1747225660@qq.com>
2019-08-07 15:27:42 +0800

    day01

---
+++ workspace/src/components/carts/ProductList.vue
@@ -0,0 +1,14 @@
+<template>
+    
+</template>
+
+<script>
+  import Product from './Product'
+    export default {
+        name: "ProductList"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ workspace/src/components/carts/ShopList.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ShopList"
+    }
+</script>
+
+<style scoped>
+
+</style>
